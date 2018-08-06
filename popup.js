cf59b43645af8f46eb38a2d0a1c62bb1c73c7a71
szovich <szovich@aol.com>
2018-08-06 12:28:38 -0400

    new commits

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
-let genderbutton = document.getElementById('gender');
-let politicalbutton = document.getElementById('political');
-let racialbutton = document.getElementById('racial');
-  chrome.storage.sync.get('color', function(data) {
-    genderbutton.style.backgroundColor = data.color;
-    genderbutton.setAttribute('value', data.color);
-    politicalbutton.style.backgroundColor = data.color;
-    politicalbutton.setAttribute('value', data.color);
-    racialbutton.style.backgroundColor = data.color;
-    racialbutton.setAttribute('value', data.color);
-  });
+// global chrome
 
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+let genderbutton = document.getElementById('gender')
+let politicalbutton = document.getElementById('political')
+let racialbutton = document.getElementById('racial')
+chrome.storage.sync.get('color', function(data) {
+  genderbutton.style.backgroundColor = data.color
+  genderbutton.setAttribute('value', data.color)
+  politicalbutton.style.backgroundColor = data.color
+  politicalbutton.setAttribute('value', data.color)
+  racialbutton.style.backgroundColor = data.color
+  racialbutton.setAttribute('value', data.color)
+})
+
+genderbutton.onclick = function (element) {
+  // change page background
+  // let color = element.target.value
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    // chrome.tabs.executeScript(
+    //     tabs[0].id,
+    //     {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    chrome.tabs.executeScript({
+      file: 'getPagesSource.js'
+    })
+  })
+}
